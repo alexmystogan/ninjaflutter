2a621ffba296e6976a64a13de5b254c69512b4db
alexmystogan <alexmystoganstore@gmail.com>
2023-04-25 09:23:26 +0700

    Test

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class NinjaCard extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.grey[900],
       appBar: AppBar(
-        title: Text('Ninja Id Card Appl'),
+        title: Text('Ninja Id Card Appal'),
         backgroundColor: Colors.grey[850],
         elevation:0.0,
       ),
